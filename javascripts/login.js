e891a301404de48c5433a6e6c96e92a862b8ed17
Todd Wacker <toddwacker@172-0-0-10.lightspeed.brhmal.sbcglobal.net>
2016-04-09 16:39:19 -0400

    Clean up code

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,11 +1,12 @@
 $('#login-submit').on('click', function() {
 	var username = $('#username-login').val(),
 		password = $('#password-login').val();
-	connection.User.please().login({instanceName: instanceName}, {username: username, password: password}).then(function(result) {
-		$('#login-message').text('User succesfully signed in! User Key is ' + result.user_key +'.');
-		// connection = new Syncano({apiKey: apiKey, userKey: result.user_key});
-	})
-	.catch(function(err) {
-		$('#login-message').text('There was an error signing in your User. Please try again.');
-	});
+	connection.User.please().login({instanceName: instanceName}, {username: username, password: password})
+		.then(function(result) {
+			$('#login-message').text('User succesfully signed in! User Key is ' + result.user_key +'.');
+			// connection = new Syncano({apiKey: apiKey, userKey: result.user_key});
+		})
+		.catch(function(err) {
+			$('#login-message').text('There was an error signing in your User. Please try again.');
+		});
 });
